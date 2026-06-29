7d0936b1f20146e53350dacf33ca98c42835a1ae
abbasodolution-web <abbasodolution@gmail.com>
2026-06-29 16:54:22 +0500

    Try to run instructor app.

--- custom-addons/odoo_error_practice_app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Error 1: Python import error.
-# The real file is practice_ticket.py, but this import uses the wrong name.
-# Learner should read the traceback and correct the import.
-from . import practice_tickets
