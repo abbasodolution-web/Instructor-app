695cd2501d7d697de56d3275d3b8373c38b1c654
abbasodolution-web <abbasodolution@gmail.com>
2026-06-30 14:26:01 +0500

    Try to run instructor app.

--- custom-addons/library_management/models/book_issue.py
+++ custom-addons/library_management/models/book_issue.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class LibraryBookIssue(models.Model):
@@ -18,10 +19,36 @@ class LibraryBookIssue(models.Model):
         ('issued', 'Issued'),
         ('returned', 'Returned'),
         ('late', 'Late'),
-    ], string="Status", default='issued')
+    ], default='issued')
 
     @api.model
     def create(self, vals):
         if vals.get('name', 'New') == 'New':
             vals['name'] = self.env['ir.sequence'].next_by_code('library.book.issue') or 'New'
+
+        book = self.env['library.book'].browse(vals.get('book_id'))
+
+        if vals.get('status', 'issued') == 'issued':
+            if book.available_copies <= 0:
+                raise ValidationError("No copies available.")
+            book.available_copies -= 1
+
         return super().create(vals)
+
+    def write(self, vals):
+        for record in self:
+            old_status = record.status
+
+            result = super().write(vals)
+
+            new_status = record.status
+
+            if old_status != 'returned' and new_status == 'returned':
+                record.book_id.available_copies += 1
+
+            elif old_status == 'returned' and new_status == 'issued':
+                if record.book_id.available_copies <= 0:
+                    raise ValidationError("No copies available.")
+                record.book_id.available_copies -= 1
+
+            return result
