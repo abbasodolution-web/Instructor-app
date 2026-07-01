cca332cfaa375e06b994a19ce89d69550360ca60
abbasodolution-web <abbasodolution@gmail.com>
2026-07-01 15:32:43 +0500

    Creating new Module Library Management.

--- custom-addons/library_management/__manifest__.py
+++ custom-addons/library_management/__manifest__.py
@@ -16,7 +16,7 @@
         'views/menu.xml',
         'views/book_views.xml',
         'views/member_views.xml',
-        'return_book_wizard_views.xml',
+        'views/return_book_wizard_views.xml',
         'views/book_issue_views.xml',
         'views/report_views.xml',
 
