a4a9039aa290e4908e213683f39cc7aa608da311
abbasodolution-web <abbasodolution@gmail.com>
2026-07-01 18:29:53 +0500

    Creating new Module Library Management.

--- custom-addons/library_management/models/book_issue.py
+++ custom-addons/library_management/models/book_issue.py
@@ -25,33 +25,9 @@ class LibraryBookIssue(models.Model):
     def create(self, vals):
         if vals.get('name', 'New') == 'New':
             vals['name'] = self.env['ir.sequence'].next_by_code('library.book.issue') or 'New'
-
-        book = self.env['library.book'].browse(vals.get('book_id'))
-
-        if vals.get('status', 'issued') == 'issued':
-            if book.available_copies <= 0:
-                raise ValidationError("No copies available.")
-            book.available_copies -= 1
-
         return super().create(vals)
 
-    def write(self, vals):
-        for record in self:
-            old_status = record.status
-
-            result = super().write(vals)
-
-            new_status = record.status
-
-            if old_status != 'returned' and new_status == 'returned':
-                record.book_id.available_copies += 1
-
-            elif old_status == 'returned' and new_status == 'issued':
-                if record.book_id.available_copies <= 0:
-                    raise ValidationError("No copies available.")
-                record.book_id.available_copies -= 1
-
-            return result
+    
     
     def action_return_book(self):
         return {
