00c6860b129877faa5187fe04b65fe8f331fdd2c
abbasodolution-web <abbasodolution@gmail.com>
2026-07-01 16:32:17 +0500

    Creating new Module Library Management.

--- custom-addons/library_management/wizard/return_book_wizard.py
+++ custom-addons/library_management/wizard/return_book_wizard.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 
 class ReturnBookWizard(models.TransientModel):
@@ -17,12 +18,15 @@ class ReturnBookWizard(models.TransientModel):
 
     remarks = fields.Text(string="Remarks")
 
+
     def action_confirm(self):
+
+        if self.book_issue_id.status == 'returned':
+            raise ValidationError("This book has already been returned.")
+
         self.book_issue_id.write({
             'status': 'returned',
             'return_date': self.return_date,
         })
-
-        self.book_issue_id.book_id.available_copies += 1
 
         return {'type': 'ir.actions.act_window_close'}
