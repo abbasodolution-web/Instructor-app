a4686de4997d5ff9d04a7207901f842339b9cd0b
abbasodolution-web <abbasodolution@gmail.com>
2026-07-03 14:26:17 +0500

    Creating new Module Library Management.

--- custom-addons/library_management/wizard/member_issue_book_wizard.py
+++ custom-addons/library_management/wizard/member_issue_book_wizard.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from odoo.exceptions import ValidationError
 
 
 class MemberIssueBookWizard(models.TransientModel):
@@ -23,5 +24,7 @@ class MemberIssueBookWizard(models.TransientModel):
             'due_date': self.due_date,
             'status': 'issued',
         })
-
+        if self.book_issue_id.book_id.available_copies <= 0:
+            raise ValidationError("No copies available.")
+        self.book_issue_id.book_id.available_copies -= 1
         return {'type': 'ir.actions.act_window_close'}
