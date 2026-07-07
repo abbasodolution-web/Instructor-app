3235f475968d7b15d7710b665e9446e9b37dd985
abbasodolution-web <abbasodolution@gmail.com>
2026-07-07 13:30:30 +0500

    Creating Book Issue Website Form in website.

--- custom-addons/library_management/__manifest__.py
+++ custom-addons/library_management/__manifest__.py
@@ -33,9 +33,9 @@
         'report/book_issue_report_template.xml',
         'report/book_issue_report_action.xml',
 
+        'website_views/website_menu.xml',
         'website_views/website_book_issue_template.xml',
         'website_views/website_book_return_template.xml',
-        'website_views/website_menu.xml',
     ],
     'installable': True,
     'application': True,
